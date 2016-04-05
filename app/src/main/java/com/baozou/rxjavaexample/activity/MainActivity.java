64f08a0447169bba24f94302350c443836e49c49
jiangyu <jiangyu@baozou.com>
2016-04-05 10:40:11 +0800

    add bottom bar

--- app/src/main/java/com/baozou/rxjavaexample/activity/MainActivity.java
+++ app/src/main/java/com/baozou/rxjavaexample/activity/MainActivity.java
@@ -1,8 +1,11 @@
 package com.baozou.rxjavaexample.activity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.IdRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -13,6 +16,8 @@ import com.baozou.rxjavaexample.common.Constants;
 import com.baozou.rxjavaexample.fragment.MainFragment;
 import com.baozou.rxjavaexample.fragment.MyCenterFragment;
 import com.baozou.rxjavaexample.fragment.QuanziFragment;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabClickListener;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -30,39 +35,28 @@ public class MainActivity extends BaseActivity {
     /* 当前Tag值 */
     private String mContentTag = "";
 
-    @Bind(R.id.icon_main)
-    ImageView iconMain;
-
-    @Bind(R.id.icon_m)
-    ImageView iconM;
-
-    @Bind(R.id.icon_e)
-    ImageView iconE;
-
-    @Bind(R.id.text_main)
-    TextView textMain;
-
-    @Bind(R.id.text_m)
-    TextView textM;
-
-    @Bind(R.id.text_e)
-    TextView textE;
-
-    @Bind(R.id.main_page)
-    RelativeLayout mainPage;
-
-    @Bind(R.id.quanzi_page)
-    RelativeLayout quanziPage;
-
-    @Bind(R.id.mycenter_page)
-    RelativeLayout myCenterPage;
+    private BottomBar bottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        initView();
+        // Bottmobar init
+        bottomBar = BottomBar.attach(this, savedInstanceState);
+        bottomBar.setItemsFromMenu(R.menu.bottombar_menu, new OnMenuTabClickListener() {
+            @Override
+            public void onMenuTabSelected(@IdRes int menuItemId) {
+                jumpFragment(menuItemId);
+            }
+
+            @Override
+            public void onMenuTabReSelected(@IdRes int menuItemId) {
+
+            }
+        });
+        bottomBar.mapColorForTab(0, ContextCompat.getColor(this, R.color.main_style_color));
+
         //执行首页跳转
         if (savedInstanceState == null) {
             FragmentTransaction tr = getSupportFragmentManager().beginTransaction();
@@ -72,12 +66,6 @@ public class MainActivity extends BaseActivity {
         }
     }
 
-    private void initView() {
-        iconMain.setImageResource(R.mipmap.icon_main_pre);
-        textMain.setTextColor(getResources().getColor(R.color.main_text_pre));
-        mainPage.getBackground().setAlpha(220);
-    }
-
     private synchronized void switchContent(Fragment fragment, String tag) {
         FragmentTransaction tr = getSupportFragmentManager().beginTransaction();
         tr.replace(R.id.content, fragment, tag);
@@ -85,116 +73,46 @@ public class MainActivity extends BaseActivity {
         tr.commitAllowingStateLoss();
     }
 
-    @OnClick(R.id.main_page)
-    void onMainPageBtnClick() {
-        String mainTag = MainFragment.TAG;
-        Fragment mainFragment = getSupportFragmentManager().findFragmentByTag(mainTag);
-        if (mainFragment == null) {
-            mainFragment = new MainFragment();
-        }
-        switchContent(mainFragment, mainTag);
-
-        iconMain.setImageResource(R.mipmap.icon_main_pre);
-        textMain.setTextColor(getResources().getColor(R.color.main_text_pre));
-        iconM.setImageResource(R.mipmap.icon_quanzi);
-        textM.setTextColor(getResources().getColor(R.color.main_text));
-        iconE.setImageResource(R.mipmap.icon_my);
-        textE.setTextColor(getResources().getColor(R.color.main_text));
-        mainPage.getBackground().setAlpha(220);
-        quanziPage.getBackground().setAlpha(255);
-        myCenterPage.getBackground().setAlpha(255);
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        bottomBar.onSaveInstanceState(outState);
     }
 
-    @OnClick(R.id.quanzi_page)
-    void onQuanziPageBtnClick() {
-        String quanziTag = QuanziFragment.TAG;
-        Fragment quanziFragment = getSupportFragmentManager().findFragmentByTag(quanziTag);
-        if (quanziFragment == null) {
-            quanziFragment = new QuanziFragment();
+    private void jumpFragment(int menuItemId) {
+        switch (menuItemId) {
+            case R.id.bb_menu_main:
+                String mainTag = MainFragment.TAG;
+                Fragment mainFragment = getSupportFragmentManager().findFragmentByTag(mainTag);
+                if (mainFragment == null) {
+                    mainFragment = new MainFragment();
+                }
+                switchContent(mainFragment, mainTag);
+                break;
+            case R.id.bb_menu_video:
+                String myCenterTag1 = MyCenterFragment.TAG;
+                Fragment myCenterFragment1 = getSupportFragmentManager().findFragmentByTag(myCenterTag1);
+                if (myCenterFragment1 == null) {
+                    myCenterFragment1 = new MyCenterFragment();
+                }
+                switchContent(myCenterFragment1, myCenterTag1);
+                break;
+            case R.id.bb_menu_quanzi:
+                String quanziTag = QuanziFragment.TAG;
+                Fragment quanziFragment = getSupportFragmentManager().findFragmentByTag(quanziTag);
+                if (quanziFragment == null) {
+                    quanziFragment = new QuanziFragment();
+                }
+                switchContent(quanziFragment, quanziTag);
+                break;
+            case R.id.bb_menu_my:
+                String myCenterTag = MyCenterFragment.TAG;
+                Fragment myCenterFragment = getSupportFragmentManager().findFragmentByTag(myCenterTag);
+                if (myCenterFragment == null) {
+                    myCenterFragment = new MyCenterFragment();
+                }
+                switchContent(myCenterFragment, myCenterTag);
+                break;
         }
-        switchContent(quanziFragment, quanziTag);
-
-        iconMain.setImageResource(R.mipmap.icon_main);
-        textMain.setTextColor(getResources().getColor(R.color.main_text));
-        iconM.setImageResource(R.mipmap.icon_quanzi_pre);
-        textM.setTextColor(getResources().getColor(R.color.main_text_pre));
-        iconE.setImageResource(R.mipmap.icon_my);
-        textE.setTextColor(getResources().getColor(R.color.main_text));
-        mainPage.getBackground().setAlpha(255);
-        quanziPage.getBackground().setAlpha(220);
-        myCenterPage.getBackground().setAlpha(255);
     }
-
-    @OnClick(R.id.mycenter_page)
-    void onMyCenterPageBtnClick() {
-        String myCenterTag = MyCenterFragment.TAG;
-        Fragment myCenterFragment = getSupportFragmentManager().findFragmentByTag(myCenterTag);
-        if (myCenterFragment == null) {
-            myCenterFragment = new MyCenterFragment();
-        }
-        switchContent(myCenterFragment, myCenterTag);
-
-        iconMain.setImageResource(R.mipmap.icon_main);
-        textMain.setTextColor(getResources().getColor(R.color.main_text));
-        iconM.setImageResource(R.mipmap.icon_quanzi);
-        textM.setTextColor(getResources().getColor(R.color.main_text));
-        iconE.setImageResource(R.mipmap.icon_my_pre);
-        textE.setTextColor(getResources().getColor(R.color.main_text_pre));
-        mainPage.getBackground().setAlpha(255);
-        quanziPage.getBackground().setAlpha(255);
-        myCenterPage.getBackground().setAlpha(220);
-    }
-
-
-//    @OnClick(R.id.btn)
-//    void onBtnClick() {
-//        GetGithubservice service = retrofit.create(GetGithubservice.class);
-//        service.listRepos("octocat")
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .unsubscribeOn(Schedulers.io())
-//                .subscribe(new Subscriber<List<Repo>>() {
-//                    @Override
-//                    public void onCompleted() {
-//
-//                    }
-//
-//                    @Override
-//                    public void onError(Throwable e) {
-//
-//                    }
-//
-//                    @Override
-//                    public void onNext(List<Repo> repos) {
-//                        btn.setText(repos.get(0).getFull_name());
-//                    }
-//                });
-//    }
-//
-//    @OnClick(R.id.btn_next)
-//    void onNextBtnClick() {
-//        GetVideosService service = retrofit.create(GetVideosService.class);
-//        service.getVideos()
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .unsubscribeOn(Schedulers.io())
-//                .subscribe(new Subscriber<DocumentListBean>() {
-//                    @Override
-//                    public void onCompleted() {
-//
-//                    }
-//
-//                    @Override
-//                    public void onError(Throwable e) {
-//
-//                    }
-//
-//                    @Override
-//                    public void onNext(DocumentListBean documentListBean) {
-//                        Log.i("retrofit_service", "size:" + documentListBean.getData().size());
-//                    }
-//                });
-//    }
-
-
 }
